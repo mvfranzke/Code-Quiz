76ba877abcc4f6f6d726b19f570fd06fa337d269
mvfranzke <michellevonfranzke@gmail.com>
2023-05-31 20:24:29 -0700

    created function to retrieve and display save data from local storage in scorelog file

--- Asset/scorelog.js
+++ Asset/scorelog.js
@@ -1,19 +1,21 @@
-function printHighscores() {
-  // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
 
-  // sort highscores by score property in descending order
+// Functions to store and display score and user name or initials
+function printHighscores() {
+  /* retrieves saved date or return empaty array if no data available */
+  let highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+  //* sorts data in decending order */
   highscores.sort(function (a, b) {
     return b.score - a.score;
   });
 
-  for (var i = 0; i < highscores.length; i += 1) {
+  for (let i = 0; i < highscores.length; i += 1) {
     // create li tag for each high score
-    var liTag = document.createElement('li');
+    let liTag = document.createElement('li');
     liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
 
     // display on page
-    var olEl = document.getElementById('highscores');
+    let olEl = document.getElementById('highscores');
     olEl.appendChild(liTag);
   }
 }
