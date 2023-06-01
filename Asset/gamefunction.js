13ff001e9d825d39dc5b2f6f216617483da19951
mvfranzke <michellevonfranzke@gmail.com>
2023-05-31 19:30:31 -0700

    set global variables

--- Asset/gamefunction.js
+++ Asset/gamefunction.js
@@ -31,6 +31,7 @@ function startQuiz() {
 }
 
 
+/* function to loop the set of questions in questionnaire js */
 function getQuestion() {
   /* returns the questions from questionnaire js */
   var currentQuestion = questions[currentQuestionIndex];
