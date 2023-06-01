ea516ef3a75223af0d077cf90f171684a6481639
mvfranzke <michellevonfranzke@gmail.com>
2023-05-31 20:26:27 -0700

    for loop to create list when displaying the new and stored scores

--- Asset/scorelog.js
+++ Asset/scorelog.js
@@ -4,17 +4,17 @@ function printHighscores() {
   /* retrieves saved date or return empaty array if no data available */
   let highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
 
-  //* sorts data in decending order */
+  /* sorts data in decending order */
   highscores.sort(function (a, b) {
     return b.score - a.score;
   });
 
+  /* for loop to create listed items when displaying saved score and initials */
   for (let i = 0; i < highscores.length; i += 1) {
-    // create li tag for each high score
     let liTag = document.createElement('li');
     liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
 
-    // display on page
+    
     let olEl = document.getElementById('highscores');
     olEl.appendChild(liTag);
   }
